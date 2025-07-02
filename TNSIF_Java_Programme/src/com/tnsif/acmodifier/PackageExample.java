d7591614756ffdaf50662b3da59673e93f01e43e
pavithra-repasitory <epavithra2502@gmail.com>
2025-07-02 20:09:48 +0530

    commit 3

---
+++ TNSIF_Java_Programme/src/com/tnsif/acmodifier/PackageExample.java
@@ -0,0 +1,10 @@
+package com.tnsif.acmodifier;
+
+public class PackageExample {
+
+	
+	public void display() {
+		System.out.println("Displaying PackageExample class");
+	}
+	
+}
---
+++ TNSIF_Java_Programme/src/com/tnsif/acmodifier/Packageinfo.java
@@ -0,0 +1,10 @@
+package com.tnsif.acmodifier;
+
+public class Packageinfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
